369e77367bc5dd3ae69913af77afd2e3340e1183
Flo2503 <bruel.florent@gmail.com>
2020-04-24 19:58:02 +0200

    Create LogIn View. Closes: #2

--- PetRescue/Log In Feature/LogInViewController.swift
+++ PetRescue/Log In Feature/LogInViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var connexionButton: UIButton!
+    @IBOutlet weak var inscriptionButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        cornerRadius([connexionButton, inscriptionButton])
     }
 
+    private func cornerRadius(_ buttons: [UIButton]) {
+        for element in buttons {
+            element.layer.cornerRadius = 22
+            element.layer.borderWidth = 3
+            element.layer.borderColor = UIColor.white.cgColor
+        }
+    }
 }
