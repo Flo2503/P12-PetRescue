288dadd90f04a856422b1f230f6f51dfb407da32
Flo2503 <bruel.florent@gmail.com>
2020-04-24 18:53:12 +0200

    add swiftlint to the project

---
+++ PetRescue/Log In Feature/LogInViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LogInViewController.swift
+//  PetRescue
+//
+//  Created by Flo on 24/04/2020.
+//  Copyright © 2020 Flo. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
