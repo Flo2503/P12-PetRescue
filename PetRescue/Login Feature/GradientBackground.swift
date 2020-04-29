da38bc372f70bf16b505364429686507172997fe
Flo2503 <bruel.florent@gmail.com>
2020-04-29 16:43:39 +0200

    Closes: #23

--- PetRescue/Login Feature/GradientBackground.swift
+++ PetRescue/Login Feature/GradientBackground.swift
@@ -18,7 +18,6 @@ extension UIView {
         gradientLayer.locations = [0.0, 1.0]
         gradientLayer.startPoint = CGPoint(x: 1.0, y: 1.0)
         gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
-
         layer.insertSublayer(gradientLayer, at: 0)
     }
 
