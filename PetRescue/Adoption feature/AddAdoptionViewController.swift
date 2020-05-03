9871e814b4effbb620a9517b4c71290dcbc26ea9
Flo2503 <bruel.florent@gmail.com>
2020-05-04 00:28:27 +0200

    add camera and camera roll feature. Closes: #11

--- PetRescue/Adoption feature/AddAdoptionViewController.swift
+++ PetRescue/Adoption feature/AddAdoptionViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AddAdoptionViewController: UIViewController {
 
+    private var imagePicker = UIImagePickerController()
+
     @IBOutlet weak var addLabel: UILabel!
     @IBOutlet weak var animalName: UITextField!
     @IBOutlet weak var animalBreed: UITextField!
@@ -17,17 +19,69 @@ class AddAdoptionViewController: UIViewController {
     @IBOutlet weak var animalAge: UITextField!
     @IBOutlet weak var locality: UITextField!
     @IBOutlet weak var infosTextView: UITextView!
-    
+    @IBOutlet weak var firstPicture: UIButton!
+    @IBOutlet weak var secondPicture: UIButton!
+    @IBOutlet weak var thirdPicture: UIButton!
+
+    @IBAction func addFirstPicture(_ sender: Any) {
+        chooseSourceImage()
+    }
+
+    @IBAction func addSecondPicture(_ sender: Any) {
+    }
+
+    @IBAction func addThirdPicture(_ sender: Any) {
+    }
+
     @IBAction func dismissKeyboard(_ sender: Any) {
         animalName.resignFirstResponder()
         animalBreed.resignFirstResponder()
         animalGender.resignFirstResponder()
         animalAge.resignFirstResponder()
         locality.resignFirstResponder()
+        infosTextView.resignFirstResponder()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         ItemSetUp.textFieldSetUp([animalName, animalBreed, animalGender, animalAge, locality])
+    }
+}
+
+extension AddAdoptionViewController {
+
+    // Photo library access
+      @objc private func libraryAccess() {
+          imagePicker.sourceType = .photoLibrary
+          imagePicker.allowsEditing = true
+          present(imagePicker, animated: true, completion: nil)
+      }
+
+      // Camera access (Bonus)
+      @objc private func cameraAccess() {
+          if UIImagePickerController.isSourceTypeAvailable(.camera) {
+              imagePicker.sourceType = .camera
+              imagePicker.allowsEditing = true
+              present(imagePicker, animated: true, completion: nil)
+          } else {
+            let alert = UIAlertController(title: "Appareil photo non valide", message: "Choisissez une photo dans la librairie", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+
+    @objc private func chooseSourceImage() {
+        let optionMenu = UIAlertController(title: "Image", message: "Choisissez une source", preferredStyle: .actionSheet)
+        let openCamera = UIAlertAction(title: "Appareil photo", style: .default, handler: { _ in
+            self.cameraAccess()
+        })
+        let openLibrary = UIAlertAction(title: "Librairie", style: .default, handler: { _ in
+            self.libraryAccess()
+        })
+        let cancelAction = UIAlertAction(title: "Annuler", style: .cancel)
+        optionMenu.addAction(openCamera)
+        optionMenu.addAction(openLibrary)
+        optionMenu.addAction(cancelAction)
+        self.present(optionMenu, animated: true, completion: nil)
     }
 }
